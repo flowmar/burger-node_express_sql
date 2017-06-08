49e606741c652cdcf627a1f2492733553cd9623b
Flowmar <omar.imam@kineticvortex.com>
2017-06-08 16:47:38 -0500

    Add depencies into package.json, Create and complete schema.sql and seeds.sql files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+--Populate the burgers database with 3 entries
+INSERT INTO burgers (id, burger_name, devoured, date)
+VALUES (1, "Portobello Swiss Burger", "true", TIMESTAMP), 
+(2, "Double Cheeseburger", true, TIMESTAMP), 
+(3, "Chili Burger", false, TIMESTAMP);
