a7d6bdd6101e22106c52cfce14fbbcbedaf44270
Flowmar <omar.imam@kineticvortex.com>
2017-06-11 15:49:53 -0500

    complete connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,5 +3,17 @@ var mysql = require('mysql');
 
 // Create connection between node.js and mysql
 var connection = mysql.connection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+});
 
-})
+connection.connect(function(err) {
+    if (err) {
+        console.error("Error connecting: " + err.stack);
+    }
+    console.log("Connected as id: " + connection.threadId);
+});
+
+module.exports = connection;
